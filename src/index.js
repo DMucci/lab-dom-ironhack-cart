5b1b393db05ab8960d6b143ee7876904c0299790
Dmucci <dmucci93@gmail.com>
2023-10-26 13:51:46 +0200

    hasta iteracion 3

--- src/index.js
+++ src/index.js
@@ -2,25 +2,47 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  console.log(product);
 
   //... your code goes here
+// Step 1
+// Almacenar en dos variables el precio y la cantidad de producto.
+const price = +product.querySelector(".price span").textContent;
+
+// Step 2. Recupera la cantidad de producto y almacenala en una variable. Le podemos llamar quantity.
+const quantity = +product.querySelector(".quantity input").value;
+
+//Step 3. Calcula el precio total y guardalo en otra variable.
+const subtotal = price * quantity;
+
+// Step 4. Actualiza el nodo del DOMO adecuado, con el precio que acabas de calcular. Tiene una class "subtotal".
+product.querySelector(".subtotal span").textContent = subtotal;
+
+//Step 5. Asegurate que esta funcion retorna el precio total calculado.
+return subtotal
+
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
-
   // ITERATION 2
-  //... your code goes here
 
-  // ITERATION 3
-  //... your code goes here
+  // Tenemos que calcular el subtotal para todos los productos
+  // 1. Usa adecuadamente document.querySelectorAll para recuperar todos los productos. selector .product
+  const products = document.querySelectorAll(".product");
+  // 2. Para cada producto, tenemos que invocar la función updateSubtotal. 
+  //Acumular el subtotal de cada producto
+  let total = 0;
+
+  products.forEach(function (p) {
+  // Tenemos que invocar la funcion updateSubtotal y pasarle como parametro el producto p 
+    
+  // ITERATION 3: Tenemos que sumar a la variable total con el subtotal caculado por la función updateSubtotal
+  total = total + updateSubtotal(p)
+});
+
+document.querySelector("#total-value span").textContent = total;
 }
-
-// ITERATION 4
+// ITERATION 4: Actualizar el nodo del DOM adecuado con el valor de la variable total
 
 function removeProduct(event) {
   const target = event.currentTarget;
